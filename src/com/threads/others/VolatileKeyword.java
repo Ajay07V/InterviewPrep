85617773af5fffbe7b60807622d21155b0ab4cf6
ajay_esuri <ajay.esuri@dellteam.com>
2025-03-19 10:33:56 +0530

    volatile keyword is used to set flags

--- src/com/threads/others/VolatileKeyword.java
+++ src/com/threads/others/VolatileKeyword.java
@@ -1,20 +1,37 @@
 package com.threads.others;
 
 public class VolatileKeyword {
-    volatile int value = 10;
+    volatile boolean value = true;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         VolatileKeyword volatileKeyword = new VolatileKeyword();
-        Thread t1 = new Thread(() -> {
-
-            volatileKeyword.value = 20;
-            System.out.println(volatileKeyword.value);
-        });
-        Thread t2 = new Thread(()->{
-            System.out.println(volatileKeyword.value);
-        });
+        Runnable writer = () -> {
+            for (int i = 0; i < 3; i++) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                volatileKeyword.value = false;
+            }
+        };
+        Runnable reader = () -> {
+            while (volatileKeyword.value) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Value is true now");
+            }
+            System.out.println("Value changed to false, exiting loop.");
+        };
+        Thread t2 = new Thread(reader);
+        Thread t1 = new Thread(writer);
         t1.start();
         t2.start();
+        t1.join();
+        t2.join();
 
     }
 }
